1d3152893974e47be2da32130e180f85bf1fb90e
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2023-12-19 11:21:41 +0530

    chore: added card with input and button

--- app/(todo)/(routes)/(root)/page.tsx
+++ app/(todo)/(routes)/(root)/page.tsx
@@ -2,8 +2,8 @@ import TodoCard from "../../_views/TodoCard";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex justify-center">
       <TodoCard />
-    </>
+    </div>
   );
 }
--- app/(todo)/_views/TodoCard.tsx
+++ app/(todo)/_views/TodoCard.tsx
@@ -3,8 +3,12 @@ import React from "react";
 
 const TodoCard = () => {
   return (
-    <div>
-      <Input />
+    <div className="card bg-base-100 shadow-xl items-center mt-4">
+      <h2 className="card-title pt-2">Todo Using Supabase</h2>
+      <div className="card-body flex-row px-2 py-6 md:p-10">
+        <Input />
+        <button className="btn btn-primary">Add Task</button>
+      </div>
     </div>
   );
 };
